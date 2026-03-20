e92c4c1685a00f88a4a72050beba876f3c248869
vladimirgar117 <vladimirgarcia117@gmail.com>
2026-03-20 15:56:27 -0600

    indicaciones: cambios de login

--- src/server.ts
+++ src/server.ts
@@ -62,3 +62,14 @@ const usuarios: Usuario[] = [
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
 });
+
+//segmentar archivo (fuera login)
+//crear una funcion para validar body. funcion para validar body
+/*el parametro recibido debe ser un objeto
+debe contener los campos username y password
+el username debe cumplir con el formato de correo (usar regex, buscarla en google o con IA)
+el password no debe estar vacio, debe tener entre 4 y 16 caracteres
+como nota adicional, los errores deben ser retornados con el mismo formato que el objeto, es decir si se recibe el siguiente body
+ */
+// validar el body con valibot,
+// repasar status , que status deberia retornar y porque ,
