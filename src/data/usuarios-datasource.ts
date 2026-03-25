24b6cdd25e9fd07c1fbc6698f34584c155a3c321
Vladimir <vladimirgarcia117@gmail.com>
2026-03-24 22:31:01 -0600

    cambios de login

--- src/data/usuarios-datasource.ts
+++ src/data/usuarios-datasource.ts
@@ -9,7 +9,7 @@ export const usuarios: Usuario[] = [
   { usuario: "admin2", password: "1A3B" },
   { usuario: "ADMIN3", password: "1S2D4" },
   { usuario: "Admin4", password: "F2E4C" },
-  { usuario: "AdmiN5", password: "12a34" },
+  { usuario: "AdmiN5@email.com", password: "12a34" },
 
 ];
  
