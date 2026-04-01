1def19ecb2c90bf34ae4c386eef8e19292027a21
Vladimir <vladimirgarcia117@gmail.com>
2026-03-31 20:42:56 -0600

    feat: add environment variable for login-validation

---
+++ src/config/regex.ts
@@ -0,0 +1,14 @@
+
+
+//cargando la variable de entorno (regex_email)
+// import 'dotenv/config';
+
+
+
+// const value = process.env.REGEX_EMAIL;
+// console.log (value);
+// if (!value) {
+//   throw new Error('La variable "EMAIL_REGEX" no está definida');
+// }
+
+//export const EMAIL_REGEX = new RegExp(value);
